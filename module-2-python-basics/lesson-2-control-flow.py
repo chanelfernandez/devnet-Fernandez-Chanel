560e0d2d6013aa5db27870ce44465fed279cee8c
Chanel Fernandez <chaneljeraldinefernandez@gmail.com>
2026-09-25 15:49:59 +0800

    Explain python conditions

--- module-2-python-basics/lesson-2-control-flow.py
+++ module-2-python-basics/lesson-2-control-flow.py
@@ -1,22 +1,34 @@
 """
 Module 2 — Lesson 2: Control Flow (if / elif / else)
-Student: [your name]
-Date: [date]
+Student: Chanel Jeraldine M. Fernandez
+Date: 9/25/2026
 
 ============================================
 WHAT IS THIS TOPIC? (explain it like you're
 teaching a friend who's never coded before)
 ============================================
-[write your own explanation here]
+Conditions are like making decisions in reality, for example if your grade is 80 or higher
+you passed, but if it is lower than 80, you failed. In Python, we use if, elif, and else to 
+tell the computer what to do depending on the situation.
+
+Condition: A rule that the computer checks
+IF: tells the computer what to do if the condition is True
+ELIF: Checks another condition if the one is false
+ELSE: Tells the computer what to do if non of the conditions are true
+comparison operator: Symbols used to compare values, such as >, <, >=, and ==.
+boolean expression: Something that gives a result of True or False.
+
+That is the purpose of Conditioning in Python
 
 
 ============================================
 KEY VOCABULARY
 ============================================
-- condition:
-- if / elif / else:
-- comparison operator:
-- boolean expression:
+IF: tells the computer what to do if the condition is True
+ELIF: Checks another condition if the one is false
+ELSE: Tells the computer what to do if non of the conditions are true
+comparison operator: Symbols used to compare values, such as >, <, >=, and ==.
+boolean expression: Something that gives a result of True or False.
 (add more as needed)
 
 
@@ -26,16 +38,19 @@ MY OWN EXAMPLE(S)
 Write at least one working example below that you
 came up with yourself — not copied from class.
 """
-
-# --- your code example goes here ---
+available = True 
+if available: 
+  print("Food is available.") 
+else: 
+  print("Food is not available.")
 
 
 """
 ============================================
 A MISTAKE I MADE (or one I want to avoid)
 ============================================
-[what's something confusing or easy to get wrong
-about this topic?]
+I want to avoid forgetting the : 
+after if and else, and I want to make sure I use the correct condition.
 
 
 ============================================
